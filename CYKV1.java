ee5ce1b8267880817fbcb72165c7b0ee516ff31a
hugovictorgutierrez <a00797974@itesm.mx>
2016-10-25 10:46:24 -0500

    correciones de la rama de carla

--- CYKV1.java
+++ CYKV1.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cyk.v1;
 
 import java.io.BufferedReader;
@@ -14,9 +20,9 @@ import java.util.Scanner;
 
 /**
  *
- * @author darkblshadox
+ * @author darkblshadox, carlations
  */
-public class CYKV1 {
+public class CYKv2 {
 
     /**
      * @param args the command line arguments
@@ -24,10 +30,8 @@ public class CYKV1 {
     static String cadenaInput;// cadena de entrada a evaluar
     static int cadenaLength;// Longitud de la cadenaInput
     static ArrayList<Character> vTerminales = new ArrayList<Character>();// Lista de elementos terminales
-    static ArrayList<Character> vNoTerminales = new ArrayList<Character>();// Lista de elementos no terminales
     static ArrayList<String> produccionesAll = new ArrayList<String>();// Lista de producciones de la gramatica
     static int numProducciones;//Numero de producciones en la gramatica
-    static int numNoTerm;//Numero de elementos no terminales
     static char ntInicial;//Cual es el simbolo de inicio de las producciones
     static String[][] producciones;//Array doble con las producciones
     //static String[][] m; //Matriz de la tabla CYK [las que sean necesarias][numero de elementos terminales]
@@ -117,29 +121,6 @@ public class CYKV1 {
 //        numElementos = listaTerminales.size();
     }
     
-    public static void addNoTerminalsInArraylist(ArrayList<Character> listanoTerminales, ArrayList<Character> listaTerminales, String[][] producciones){// Agrega los elemento de la cadena dentro de la  listaArray si aun no son agregado
-        char letter;
-        String vals;
-        for(int i=0; i<producciones.length; i++){
-            for(int j=0; j<producciones[i].length; j++){
-                vals=producciones[i][j];
-                for(int k=0; k<vals.length();k++){
-                    letter=vals.charAt(k);
-                    if(isTerminalInArray(listanoTerminales,letter)==false && isTerminalInArray(listaTerminales,letter)==false){//Si el elemento no esta en la lista de vTerminales
-                        listanoTerminales.add(letter);
-                    }
-                }
-            }
-        }
-        
-        //Muestra los elementos dentro de la arraylist
-        System.out.print("Elementos no terminales: ");
-        printArrayCharlist(listanoTerminales);
-        numNoTerm=listanoTerminales.size();
-         //Define el numero de elementos en el arraylist
-//        numElementos = listaTerminales.size();
-    }
-    
     public static void printArrayCharlist(ArrayList<Character> arrayList){//Imprimir contenidos del arraylist de caracteres
         for(int i=0; i<arrayList.size();i++){
             System.out.print(arrayList.get(i) + ", ");
@@ -154,16 +135,6 @@ public class CYKV1 {
 //        System.out.println("\nFin de Arraylist.");
     }
     
-//    public static void searchQo(char q0){//Verifica si el q0 previamente definido si esta en las producciones
-//        String val = ""+q0;
-//        for(int i = 0; i < producciones.length; i++){
-//            if(producciones[i][0]!=val){
-//                System.out.println("ERROR: El q0 previamente definido no esta en las producciones.\nTerminando programa.");
-//                System.exit(0);
-//            }
-//        }
-//    }
-    
     public static void addGrammar (int numofPs){//Agrega las producciones de la gramatica al arraylist produccionesAll desde input
         Scanner pr = new Scanner(System.in);
         String input;
@@ -177,22 +148,44 @@ public class CYKV1 {
     
     }
     
-    public static void CYKalgorithm(){
-    
+    public static String[][] CYKalgorithm(int num, ArrayList<Character> vT, String[][] prod, char ntIni, String cinput){
+        String[][] tablares = new String[num][num];
+        for(int i = 0; i < num; i++){
+            tablares[i][i+1] = "";
+            for(int j = 0; j < num-1; j++){
+                if(prod[j][1].length() == 1 && cinput.substring(i, i+1).equals(prod[j][1])){
+                    tablares[i][i+1] += prod[j][0];
+                }
+            }
+        }
+        
+        for(int m = 0; m <= num; m++){
+            for(int i = 0; i <= num-m; i++){
+                tablares[i][i+m] = "";
+                for(int j = i+1; j < (i+m-1); j++){
+                    for(int f = 0; f <= num; f++){
+                        if(prod[f][1].length() == 2 && tablares[i][j].contains(prod[f][1].subSequence(0, 0)) && tablares[j][i+m].contains(prod[f][1].subSequence(0, 0))){
+                            tablares[i][i+m] += prod[f][0];
+                        }
+                    }
+                }
+            }
+        }
+        return tablares;
     }
     
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner in = new Scanner(System.in);
-        System.out.print("Proyecto #2: Algoritmo CYK\n\nCual es la cadena a evaluar: ");
-        String filename = "producciones.txt";// Nombre del archivo externo de donde se obtiene las producciones
+        System.out.print("Proyecto #2: Algoritmo CYK\n\nCual es la cadena a evaluar:");
+        String filename = "producciones.txt";
 
         //Cadena
         cadenaInput = in.nextLine();//Recibe la cadena a evaluar
         cadenaLength = cadenaInput.length();//Define la longitud de la cadena
         
         //Elementos terminales
-        System.out.print("\nElementos Terminales en cadena: ");
+        System.out.print("\nElementos Terminales: ");
         addElementsInArraylist(vTerminales,cadenaInput);//Llena la lista de elementos Terminales con la cadena a evaluar y define el numero de elementos terminales
         System.out.println("\nNumero de elementos Terminales = " + vTerminales.size());
        
@@ -207,18 +200,16 @@ public class CYKV1 {
         ntInicial = in.nextLine().charAt(0);// Se defina el elemento inicial
         numProducciones = countProd(filename);//Se cuenta la cantidad de producciones desde el archivo externo
         archivoDeProduc(filename);//Lee el archivo externo para guardar las producciones en el doublearray producciones
-        System.out.println("\nNumero de producciones dentro del archivo '" + filename + "' = "+numProducciones);
-        addNoTerminalsInArraylist(vNoTerminales,vTerminales,producciones);//Obtiene los elementos no terminales y cuenta el numero de no terminales
-        System.out.println("\nNumero de elementos no terminales = "+numNoTerm);
-        
+       //System.out.println("Numero de producciones = "+numProducciones);
+
         //Desplegar datos obtenidos
-        System.out.println("\n\nq0 = " + ntInicial + "\n\nP = ");
+        System.out.println("\nq0 = " + ntInicial + "\n\nP = ");
         printArray(producciones);
-        //searchQo(ntInicial);
         System.out.println("\nPara la cadena: " + cadenaInput);
         System.out.println("Longitud de cadena de entrada=" + cadenaLength);
         
-        //ejecucion...
+        //Se necesita metodo de concatenacion
+        CYKalgorithm(numProducciones, vTerminales, producciones, ntInicial, cadenaInput);
     }
     
 }
